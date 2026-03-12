76272471c6a6bff7025b697e9918c479bde219af
Hossam Hassan <148561274+Borutlee@users.noreply.github.com>
2026-03-12 15:13:02 +0200

    Update base path in Vite configuration
    Change base path for deployment on Vercel.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Golden-Flower/",
+  base: './', // مهم جداً للـ root deployment على Vercel
 })
