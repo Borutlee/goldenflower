1a5e81435136bc0bd0144f50190eb0faef72faae
Hossam Hassan <148561274+Borutlee@users.noreply.github.com>
2026-05-14 22:25:09 +0300

    Add files via upload

--- src/Components/ProtectedAdminRoute.jsx
+++ src/Components/ProtectedAdminRoute.jsx
@@ -5,9 +5,7 @@ export default function ProtectedAdminRoute({ children }) {
     const { user, loading } = useAuth();
 
     if (loading) return null;
-
     if (!user) return <Navigate to="/Auth" replace />;
-
     if (user?.user_metadata?.role !== 'admin') return <Navigate to="/" replace />;
 
     return children;
--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -4,12 +4,8 @@ import { Navigate } from 'react-router-dom';
 export default function ProtectedRoute({ children }) {
     const { user, loading } = useAuth();
 
-    // لو لسه بيجيب بيانات اليوزر استنى
     if (loading) return null;
-
-    // لو مش logged in وديه على Auth
     if (!user) return <Navigate to="/Auth" replace />;
 
-    // لو logged in وريه الصفحة
     return children;
 }
